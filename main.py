90185197bad96083d050c14108447dcffb03ae5e
MariaEinstein <99494653+MariaEinstein@users.noreply.github.com>
2022-07-11 17:52:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from functions import search_users, get_photo, json_create
 from db_vk import engine, Session, write_msg, register_user, add_user_photos, check_db_master
-
+#token group(вставляем нужный)
 
 
 #vk_api
